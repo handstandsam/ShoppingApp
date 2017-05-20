78ed105d53e1f5efaaf2ea92c9da8f4ff755b419
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2017-05-20 11:58:54 -0400

    Default to ProduceMockAccount

--- app/src/main/java/com/handstandsam/maintainableespresso/MyAbstractApplication.java
+++ app/src/main/java/com/handstandsam/maintainableespresso/MyAbstractApplication.java
@@ -3,8 +3,8 @@ package com.handstandsam.maintainableespresso;
 import android.app.Application;
 
 import com.handstandsam.maintainableespresso.di.AppComponent;
+import com.handstandsam.maintainableespresso.mockaccount.ProduceMockAccount;
 import com.handstandsam.maintainableespresso.mockaccount.Stubberator;
-import com.handstandsam.maintainableespresso.mockaccount.VideoGameMockAccount;
 
 public abstract class MyAbstractApplication extends Application {
 
@@ -14,8 +14,8 @@ public abstract class MyAbstractApplication extends Application {
     public void onCreate() {
         super.onCreate();
         appComponent = createAppComponent();
-//        new Stubberator(this).stubItAll(new ProduceMockAccount());
-        new Stubberator(this).stubItAll(new VideoGameMockAccount());
+        new Stubberator(this).stubItAll(new ProduceMockAccount());
+//        new Stubberator(this).stubItAll(new VideoGameMockAccount());
     }
 
     protected abstract AppComponent createAppComponent();
