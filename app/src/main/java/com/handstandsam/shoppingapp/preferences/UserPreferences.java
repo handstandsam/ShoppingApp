af8b98da8ee71884945c670278213df0d9bf5160
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2017-08-12 13:30:47 -0400

    Added convenience method.

--- app/src/main/java/com/handstandsam/shoppingapp/preferences/UserPreferences.java
+++ app/src/main/java/com/handstandsam/shoppingapp/preferences/UserPreferences.java
@@ -61,6 +61,10 @@ public class UserPreferences {
         return null;
     }
 
+    public void clearRememberMe() {
+        setRememberMe(false, null);
+    }
+
     public void setRememberMe(boolean rememberMe, String username) {
         sharedPreferences.edit().putBoolean(REMEMBER_ME, rememberMe).apply();
         if (rememberMe) {
