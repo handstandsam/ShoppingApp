7d32d006c804ebaabe9457fe62337dc0420567ab
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2017-09-29 22:06:50 -0400

    Kotlinifying

---
+++ app/src/main/java/com/handstandsam/shoppingapp/features/category/CategoryActivity.kt
@@ -0,0 +1,77 @@
+package com.handstandsam.shoppingapp.features.category
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import butterknife.ButterKnife
+import com.handstandsam.shoppingapp.LoggedInActivity
+import com.handstandsam.shoppingapp.MyAbstractApplication
+import com.handstandsam.shoppingapp.R
+import com.handstandsam.shoppingapp.models.Item
+
+class CategoryActivity : LoggedInActivity() {
+
+    lateinit var recyclerView: RecyclerView
+
+    private var recyclerViewAdapter: CategoryRVAdapter? = null
+
+    private var view: CategoryView? = null
+
+    private var presenter: CategoryPresenter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        ButterKnife.bind(this)
+        recyclerView = findViewById(R.id.categories)
+        (application as MyAbstractApplication).appComponent.inject(this)
+
+        recyclerView!!.layoutManager = GridLayoutManager(this, 2)
+        recyclerViewAdapter = CategoryRVAdapter()
+        recyclerView!!.adapter = recyclerViewAdapter
+        view = MyCategoryView()
+        presenter = CategoryPresenter(view!!)
+    }
+
+    interface CategoryView {
+
+        val context: Context
+
+        fun showItems(items: List<Item>)
+
+        fun setActionBarTitle(title: String)
+
+        fun showNetworkError(message: String)
+    }
+
+    inner class MyCategoryView : CategoryView {
+
+        override val context: Context
+            get() = this@CategoryActivity.applicationContext
+
+        override fun showItems(items: List<Item>) {
+            recyclerViewAdapter!!.setItems(items)
+        }
+
+        override fun setActionBarTitle(title: String) {
+            supportActionBar!!.title = title
+        }
+
+        override fun showNetworkError(message: String) {
+            val builder = AlertDialog.Builder(
+                    this@CategoryActivity).setTitle("Networking Error")
+            if (message != null) {
+                builder.setMessage(message)
+            }
+            builder.setPositiveButton("OK") { dialogInterface, i -> this@CategoryActivity.finish() }.show()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter!!.onResume(intent)
+    }
+}
---
+++ app/src/main/java/com/handstandsam/shoppingapp/features/checkout/CheckoutActivity.kt
@@ -0,0 +1,67 @@
+package com.handstandsam.shoppingapp.features.checkout
+
+import android.os.Bundle
+import android.view.Menu
+import android.widget.TextView
+import com.handstandsam.shoppingapp.LoggedInActivity
+import com.handstandsam.shoppingapp.MyAbstractApplication
+import com.handstandsam.shoppingapp.R
+import com.handstandsam.shoppingapp.di.AppComponent
+
+class CheckoutActivity : LoggedInActivity() {
+
+    lateinit var itemCountTextView: TextView
+
+    lateinit var itemsText: TextView
+
+    private var view: CheckoutView? = null
+
+    private var presenter: CheckoutPresenter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar!!.setTitle(R.string.cart)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        setContentView(R.layout.activity_checkout)
+        itemCountTextView = findViewById<TextView>(R.id.item_count)
+        itemsText = findViewById<TextView>(R.id.items)
+        (application as MyAbstractApplication).appComponent.inject(this)
+
+        view = MyCheckoutView()
+        presenter = CheckoutPresenter(view!!)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.checkout_menu, menu)
+        return true
+    }
+
+    interface CheckoutView {
+
+        val appComponent: AppComponent
+
+        fun setItemCountText(text: String)
+
+        fun setItemsText(text: String)
+    }
+
+    inner class MyCheckoutView : CheckoutView {
+
+        override val appComponent: AppComponent
+            get() = (application as MyAbstractApplication).appComponent
+
+        override fun setItemCountText(text: String) {
+            itemCountTextView.text = text
+        }
+
+        override fun setItemsText(text: String) {
+            itemsText.text = text
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter!!.onResume()
+    }
+}
