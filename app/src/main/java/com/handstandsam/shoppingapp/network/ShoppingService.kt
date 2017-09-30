b76e2143e5ea0af757bd1c77d9aefa2cb8352f8b
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2017-09-29 21:32:37 -0400

    Converted Retrofit to Kotlin

---
+++ app/src/main/java/com/handstandsam/shoppingapp/network/ShoppingService.kt
@@ -0,0 +1,23 @@
+package com.handstandsam.shoppingapp.network
+
+import com.handstandsam.shoppingapp.models.Category
+import com.handstandsam.shoppingapp.models.Item
+import com.handstandsam.shoppingapp.models.LoginRequest
+import com.handstandsam.shoppingapp.models.User
+import io.reactivex.Single
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface ShoppingService {
+
+    @POST("login")
+    fun login(@Body loginRequest: LoginRequest): Single<User>
+
+    @get:GET("categories")
+    val categories: Single<List<Category>>
+
+    @GET("category/{categoryName}/items")
+    fun getItemsForCategory(@Path("categoryName") categoryName: String): Single<List<Item>>
+}
