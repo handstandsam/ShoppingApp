17b97ad46d56d3c1c6c5413369d92596ffe50b92
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2017-09-29 21:55:11 -0400

    Kotlin

---
+++ app/src/debug/java/com/handstandsam/shoppingapp/NetworkConfig.kt
@@ -0,0 +1,38 @@
+package com.handstandsam.shoppingapp
+
+import android.content.Context
+
+import com.handstandsam.shoppingapp.di.NetworkModule
+import com.handstandsam.shoppingapp.mockaccount.MockAccount
+import com.handstandsam.shoppingapp.mockaccount.Stubberator
+import com.handstandsam.shoppingapp.wiremock.WireMockManager
+
+
+internal class NetworkConfig(var applicationContext: Context) {
+
+    fun startNormally() {}
+
+    fun stubLocalWireMock(mockAccount: MockAccount) {
+        NetworkModule.USE_LOCAL_SERVER = true
+        Stubberator(applicationContext).stubItAll(mockAccount)
+    }
+
+    fun stubRemoteWireMock(mockAccount: MockAccount) {
+        NetworkModule.USE_LOCAL_SERVER = false
+        Stubberator(applicationContext).stubItAll(mockAccount)
+    }
+
+
+    fun recordMappingsAndProxy(remoteBaseUrl: String) {
+        //                Record
+        val wireMockManager = WireMockManager.Builder(applicationContext, NetworkModule.LOCALHOST_PORT).build()
+        wireMockManager.startProxyAndRecord(remoteBaseUrl)
+    }
+
+
+    fun playbackRecordedMappings() {
+        //                Playback
+        val wireMockManager = WireMockManager.Builder(applicationContext, NetworkModule.LOCALHOST_PORT).build()
+        wireMockManager.startPlayBack()
+    }
+}
