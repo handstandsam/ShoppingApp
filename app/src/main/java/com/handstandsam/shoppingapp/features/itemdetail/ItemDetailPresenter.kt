fd27a2e33cfff9c87e3262639ea765bc5dea1fc9
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2017-09-29 22:34:22 -0400

    Kotlin

---
+++ app/src/main/java/com/handstandsam/shoppingapp/features/itemdetail/ItemDetailPresenter.kt
@@ -0,0 +1,43 @@
+package com.handstandsam.shoppingapp.features.itemdetail
+
+import android.content.Intent
+import com.handstandsam.shoppingapp.models.Item
+import com.handstandsam.shoppingapp.repository.CheckoutCart
+import com.handstandsam.shoppingapp.repository.SessionManager
+import javax.inject.Inject
+
+class ItemDetailPresenter(private val view: ItemDetailActivity.ItemDetailView) {
+
+    @Inject
+    lateinit internal var sessionManager: SessionManager
+
+    @Inject
+    lateinit internal var cart: CheckoutCart
+
+    lateinit internal var item: Item
+
+    init {
+        view.appComponent.inject(this)
+    }
+
+    fun onResume(intent: Intent) {
+
+        val extras = intent.extras
+        val item = extras!!.get(BUNDLE_PARAM_ITEM) as Item
+        this.item = item
+
+        view.setLabel(item.label!!)
+        view.setImageUrl(item.image!!)
+        view.setActionBarTitle(item.label!!)
+    }
+
+    fun addToCardClicked() {
+        cart.addItem(item)
+        view.showToast(item.label!! + " added to cart.")
+    }
+
+    companion object {
+
+        val BUNDLE_PARAM_ITEM = "item"
+    }
+}
