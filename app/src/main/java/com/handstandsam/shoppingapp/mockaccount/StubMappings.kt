199c2ec8faa9de7b873a26b97ebac99bba97e648
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2017-09-29 21:39:05 -0400

    Converting to Kotlin

---
+++ app/src/main/java/com/handstandsam/shoppingapp/mockaccount/StubMappings.kt
@@ -0,0 +1,25 @@
+package com.handstandsam.shoppingapp.mockaccount
+
+
+import android.net.Uri
+import com.github.tomakehurst.wiremock.client.MappingBuilder
+import com.github.tomakehurst.wiremock.client.WireMock.*
+
+object StubMappings {
+
+
+    val categories: MappingBuilder
+        @JvmStatic
+        get() = get(urlEqualTo("/categories"))
+
+    @JvmStatic
+    fun getItemsForCategory(categoryId: String): MappingBuilder {
+        return get(urlEqualTo("/category/" + Uri.encode(categoryId) + "/items"))
+    }
+
+    @JvmStatic
+    fun login(): MappingBuilder {
+        return post(urlEqualTo("/login"))
+    }
+
+}
