42a202f5e49bed9617b5174b161d2f5d0ac27c2a
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2017-09-29 20:56:49 -0400

    Converted to Kotlin

---
+++ app/src/main/java/com/handstandsam/shoppingapp/utils/IntentUtils.kt
@@ -0,0 +1,40 @@
+package com.handstandsam.shoppingapp.utils
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.handstandsam.shoppingapp.R
+import com.handstandsam.shoppingapp.features.login.LoginActivity
+
+object IntentUtils {
+
+    @JvmStatic
+    fun getIntentForUsername(context: Context, username: String): Intent {
+        val intent = Intent(context, LoginActivity::class.java)
+        val bundle = Bundle()
+        bundle.putString("username", username)
+        intent.putExtras(bundle)
+        return intent
+    }
+
+    @JvmStatic
+    fun triggerPRNotification(context: Context, username: String) {
+        val id = System.currentTimeMillis().toInt()
+        val pIntent = PendingIntent.getActivity(context, id, getIntentForUsername(context, username), PendingIntent.FLAG_ONE_SHOT)
+
+        val n = Notification.Builder(context)
+                .setContentTitle("New Order from " + username)
+                .setContentText("Added Debug Features to App.")
+                .setSmallIcon(R.drawable.ic_shopping_cart_white_24px)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true).build()
+
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify(id, n)
+    }
+
+}
