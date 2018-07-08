9030e1a1e3bc3ee065ba5e2d57fe97842a04e961
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2018-07-08 13:08:48 -0400

    Kotlin + buildSrc cleanup.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,7 +2,7 @@
 
 object Projects {
     val models = ":models"
-    val mockData = ":mock-data"
+    val mock_data = ":mock-data"
     val sample = ":sample"
 }
 
@@ -11,15 +11,15 @@ object Versions {
     val rxjava = "2.1.9"
     val source_compat = "1.8"
     val target_compat = "1.8"
-    val minSdk = 21
-    val targetSdk = 27
-    val compileSdk = 27
-    val buildTools = "27.0.3"
-
+    val min_sdk = 21
+    val target_sdk = 27
+    val compile_sdk = 27
+    val build_tools = "27.0.3"
 
     // Kotlin
     val kotlin = "1.2.51"
-    val kotlin_coroutines = "0.22.1"
+    val kotlin_coroutines = "0.23.4"
+    val apache_commons = "2.4"
 
     val stetho = "1.5.0"
     val okhttp = "3.10.0"
@@ -42,6 +42,7 @@ object Versions {
 }
 
 object Libs {
+    val support_recycler_view = "com.android.support:recyclerview-v7:${Versions.support_lib}"
     val support_annotations = "com.android.support:support-annotations:${Versions.support_lib}"
     val support_appcompat_v7 = "com.android.support:appcompat-v7:${Versions.support_lib}"
     val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
@@ -50,63 +51,46 @@ object Libs {
     val rx_android = "io.reactivex.rxjava2:rxandroid:${Versions.rx_android}"
     val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
     val retrofit_moshi_converter = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
-    val processPhoenix = "com.jakewharton:process-phoenix:${Versions.process_phoenix}"
+    val process_phoenix = "com.jakewharton:process-phoenix:${Versions.process_phoenix}"
+    val support_v4 = "com.android.support:support-v4:${Versions.support_lib}"
+    val support_design = "com.android.support:design:${Versions.support_lib}"
 
     val dagger = "com.google.dagger:dagger:${Versions.dagger}"
     val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
-    val daggerKapt = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+    val dagger_kapt = "com.google.dagger:dagger-compiler:${Versions.dagger}"
     val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
-    val okHttp3LoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}"
-    val okHttp3MockWebServer = "com.squareup.okhttp3:mockwebserver:${Versions.okhttp}"
-    val retrofitConverterGson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
-    val retrofitAdapterRxJava2 = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+    val okhttp_logging_interceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}"
+    val okhttp_mockwebserver = "com.squareup.okhttp3:mockwebserver:${Versions.okhttp}"
+    val retrofit_converter_gson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+    val retrofit_adapter_rxjava2 = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
     val moshi = "com.squareup.moshi:moshi:${Versions.moshi}"
     val moshiKotlin = "com.squareup.moshi:moshi-kotlin:${Versions.moshi}"
     val wiremock = "com.github.tomakehurst:wiremock:${Versions.wiremock}"
     val apacheHttpClientAndroid = "org.apache.httpcomponents:httpclient-android:4.3.5.1"
-    val rxJava2 = "io.reactivex.rxjava2:rxjava:${Versions.rx_android}"
+    val kotlin_std_lib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
 
-    val runner = "com.android.support.test:runner:1.0.1"
-    val orchestrator = "com.android.support.test:orchestrator:1.0.1"
-    val espressoCore = "com.android.support.test.espresso:espresso-core:3.0.1"
-    val espressoContrib = "com.android.support.test.espresso:espresso-contrib:3.0.1"
-
-    val kotlin_reflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
+   val kotlin_reflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
     val coroutines =
         "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.kotlin_coroutines}"
-    val coroutinesAndroid =
+    val coroutines_android =
         "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.kotlin_coroutines}"
-
-    val junit = "junit:junit:${Versions.junit}"
-
-    val compat = "com.android.support:support-compat:${Versions.support_lib}"
-    val coreUtils = "com.android.support:support-core-utils:${Versions.support_lib}"
-    val percent = "com.android.support:percent:${Versions.support_lib}"
-    val supportAnnotations = "com.android.support:support-annotations:${Versions.support_lib}"
-    val supportFragment = "com.android.support:support-fragment:${Versions.support_lib}"
-    val supportV4 = "com.android.support:support-v4:${Versions.support_lib}"
-    val supportV13 = "com.android.support:support-v13:${Versions.support_lib}"
-    val supportAppCompatV7 = "com.android.support:appcompat-v7:${Versions.support_lib}"
-    val supportRecyclerView = "com.android.support:recyclerview-v7:${Versions.support_lib}"
-    val supportCardView = "com.android.support:cardview-v7:${Versions.support_lib}"
-    val supportGridLayout = "com.android.support:gridlayout-v7:${Versions.support_lib}"
-    val supportDesign = "com.android.support:design:${Versions.support_lib}"
-    val supportPreferenceV7 = "com.android.support:preference-v7:${Versions.support_lib}"
-    val supportPreferenceV14 = "com.android.support:preference-v14:${Versions.support_lib}"
-    val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
 
     val timber = "com.jakewharton.timber:timber:${Versions.timber}"
     val stetho = "com.facebook.stetho:stetho:${Versions.stetho}"
-    val stethoOkHttp3 = "com.facebook.stetho:stetho-okhttp3:${Versions.stetho}"
+    val stetho_okhttp3 = "com.facebook.stetho:stetho-okhttp3:${Versions.stetho}"
     val chuck = "com.readystatesoftware.chuck:library:${Versions.chuck}"
-    val chuckNoOp = "com.readystatesoftware.chuck:library-no-op:${Versions.chuck}"
+    val chuck_no_op = "com.readystatesoftware.chuck:library-no-op:${Versions.chuck}"
+    val apache_commons = "commons-io:commons-io:${Versions.apache_commons}"
 }
 
 object TestLibs {
-    val espresso = "com.android.support.test.espresso:espresso-core:${Versions.espresso}"
-    val espressoIntents = "com.android.support.test.espresso:espresso-intents:${Versions.espresso}"
-    val androidTestRunner = "com.android.support.test:runner:${Versions.test_support_lib}"
+    val runner = "com.android.support.test:runner:${Versions.test_support_lib}"
+    val orchestrator = "com.android.support.test:orchestrator:${Versions.test_support_lib}"
+    val espresso_core = "com.android.support.test.espresso:espresso-core:${Versions.espresso}"
+    val espresso_contrib = "com.android.support.test.espresso:espresso-contrib:${Versions.espresso}"
+    val android_test_runner = "com.android.support.test:runner:${Versions.test_support_lib}"
     val testRules = "com.android.support.test:rules:${Versions.test_support_lib}"
+    val junit = "junit:junit:${Versions.junit}"
 }
 
 object GradlePlugins {
@@ -115,7 +99,7 @@ object GradlePlugins {
 }
 
 object Modules {
-    val supportAnnotations = mapOf(
+    val support_annotations = mapOf(
         GROUP to "com.android.support",
         MODULE to "support-annotations"
     )
