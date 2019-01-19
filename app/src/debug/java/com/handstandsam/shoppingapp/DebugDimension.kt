f81ac993cccc8d5f7b9c482383252089a0b8a9a7
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2019-01-19 14:35:54 -0500

    Chuck is always enabled now.

--- app/src/debug/java/com/handstandsam/shoppingapp/DebugDimension.kt
+++ app/src/debug/java/com/handstandsam/shoppingapp/DebugDimension.kt
@@ -2,7 +2,6 @@ package com.handstandsam.shoppingapp
 
 import android.content.Context
 import com.facebook.stetho.okhttp3.StethoInterceptor
-import com.handstandsam.shoppingapp.debug.DebugPreferences
 import com.readystatesoftware.chuck.ChuckInterceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -11,9 +10,6 @@ fun OkHttpClient.Builder.debugDimensionAddInterceptors(appContext: Context): OkH
     this
         .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
         .addInterceptor(StethoInterceptor())
-
-    if (DebugPreferences(appContext).isChuckEnabled) {
-        this.addInterceptor(ChuckInterceptor(appContext))
-    }
+        .addInterceptor(ChuckInterceptor(appContext))
     return this
 }
