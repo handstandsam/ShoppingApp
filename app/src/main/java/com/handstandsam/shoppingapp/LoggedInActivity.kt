4159f70596120a5e3a16f59431d9c740219b0f0f
Sam Edwards <sam@handstandtech.com>
2019-03-30 21:59:43 -0400

    Added Shopping Cart Badge with Item Count (#12)
    * Got shopping cart badge working!
    
    * Added Coroutines to expose the items int the cart and update the UI.
    
    * Sending around the items in the cart, not just the count.

--- app/src/main/java/com/handstandsam/shoppingapp/LoggedInActivity.kt
+++ app/src/main/java/com/handstandsam/shoppingapp/LoggedInActivity.kt
@@ -6,12 +6,21 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
 import com.handstandsam.shoppingapp.di.AppGraph
 import com.handstandsam.shoppingapp.features.checkout.CheckoutActivity
 import com.handstandsam.shoppingapp.features.login.LoginActivity
 import com.handstandsam.shoppingapp.repository.SessionManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
 
-open class LoggedInActivity : AppCompatActivity() {
+
+open class LoggedInActivity : AppCompatActivity(),
+    CoroutineScope by CoroutineScope(Dispatchers.Default) {
 
     protected val graph: AppGraph get() = application.graph()
 
@@ -26,9 +35,36 @@ open class LoggedInActivity : AppCompatActivity() {
         }
     }
 
+    lateinit var redCircle: FrameLayout
+    lateinit var countTextView: TextView
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.logged_in_menu, menu)
-        return super.onCreateOptionsMenu(menu)
+        val alertMenuItem = menu.findItem(R.id.activity_main_alerts_menu_item)
+        val rootView = (alertMenuItem.actionView as FrameLayout)
+
+        redCircle = rootView.findViewById(R.id.view_alert_red_circle)
+
+        countTextView = rootView.findViewById(R.id.view_alert_count_textview)
+        rootView.setOnClickListener {
+            startActivity(Intent(this, CheckoutActivity::class.java))
+        }
+
+        launch {
+            graph.sessionGraph.checkoutCart.itemsInCartStream().consumeEach { itemsInCart ->
+                runOnUiThread {
+                    val itemsInCartCount = itemsInCart.size
+                    if (itemsInCartCount == 0) {
+                        redCircle.visibility = View.GONE
+                    } else {
+                        redCircle.visibility = View.VISIBLE
+                        countTextView.text = itemsInCartCount.toString()
+                    }
+                }
+            }
+        }
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -37,10 +73,6 @@ open class LoggedInActivity : AppCompatActivity() {
                 sessionManager.logout()
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
-                return true
-            }
-            R.id.view_cart -> {
-                startActivity(Intent(this, CheckoutActivity::class.java))
                 return true
             }
             android.R.id.home -> {
--- app/src/main/java/com/handstandsam/shoppingapp/repository/CheckoutCart.kt
+++ app/src/main/java/com/handstandsam/shoppingapp/repository/CheckoutCart.kt
@@ -1,8 +1,17 @@
 package com.handstandsam.shoppingapp.repository
 
 import com.handstandsam.shoppingapp.models.Item
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.launch
 
-data class CheckoutCart(val itemsInCart: MutableList<Item> = mutableListOf()) {
+data class CheckoutCart(val itemsInCart: MutableList<Item> = mutableListOf()) :
+    CoroutineScope by CoroutineScope(Dispatchers.Default) {
+
+
+    private val channel = ConflatedBroadcastChannel<List<Item>>(itemsInCart.toList())
 
     fun empty() {
         itemsInCart.clear()
@@ -10,12 +19,19 @@ data class CheckoutCart(val itemsInCart: MutableList<Item> = mutableListOf()) {
 
     fun addItem(item: Item) {
         itemsInCart.add(item)
+        launch {
+            channel.send(itemsInCart.toList())
+        }
     }
 
     fun removeItem(item: Item) {
         itemsInCart.remove(item)
     }
 
+    fun itemsInCartStream(): ReceiveChannel<List<Item>> {
+        return channel.openSubscription()
+    }
+
     val items: List<Item>
-        get() = itemsInCart
+        get() = itemsInCart.toList()
 }
