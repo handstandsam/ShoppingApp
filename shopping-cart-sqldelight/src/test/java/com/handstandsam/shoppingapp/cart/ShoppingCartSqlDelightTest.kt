e7539c451e2e8a2d3119ce8ec34b9fdebd808cc7
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2019-04-02 09:02:24 -0400

    Too much whitespace

--- shopping-cart-sqldelight/src/test/java/com/handstandsam/shoppingapp/cart/ShoppingCartSqlDelightTest.kt
+++ shopping-cart-sqldelight/src/test/java/com/handstandsam/shoppingapp/cart/ShoppingCartSqlDelightTest.kt
@@ -9,7 +9,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 
-
 class ShoppingCartSqlDelightTest {
 
     private lateinit var testDelegate: TestDelegate
