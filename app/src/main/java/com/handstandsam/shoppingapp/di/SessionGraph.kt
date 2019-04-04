a9e9df83572178fac9380e81f8092c618d9afb6c
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2019-04-03 21:17:40 -0400

    Modified to match the new `ShoppingCartDao` pattern to remove the business logic from the `Dao`.  Also updated tests and added documentation.

--- app/src/main/java/com/handstandsam/shoppingapp/di/SessionGraph.kt
+++ app/src/main/java/com/handstandsam/shoppingapp/di/SessionGraph.kt
@@ -1,14 +1,12 @@
 package com.handstandsam.shoppingapp.di
 
 import android.content.Context
-import com.handstandsam.shoppingapp.cart.InMemoryShopingCartDao
 import com.handstandsam.shoppingapp.cart.ShoppingCart
-import com.handstandsam.shoppingapp.cart.ShoppingCartSqlDelight
+import com.handstandsam.shoppingapp.cart.SqlDelightShoppingCartDao
 import com.handstandsam.shoppingapp.cart.sqldelight.Database
 import com.handstandsam.shoppingapp.preferences.UserPreferences
 import com.handstandsam.shoppingapp.repository.SessionManager
 import com.squareup.sqldelight.android.AndroidSqliteDriver
-import com.squareup.sqldelight.db.SqlDriver
 
 interface SessionGraph {
     val sessionManager: SessionManager
@@ -20,13 +18,15 @@ class SessionGraphImpl(
     appContext: Context
 ) : SessionGraph {
 
-    private val sqlDriver: SqlDriver = AndroidSqliteDriver(
-        schema = Database.Schema,
-        context = appContext,
-        name = "cart_sqldelight.db"
+    override val shoppingCart: ShoppingCart = ShoppingCart(
+        SqlDelightShoppingCartDao(
+            AndroidSqliteDriver(
+                schema = Database.Schema,
+                context = appContext,
+                name = "cart_sqldelight.db"
+            )
+        )
     )
-
-    override val shoppingCart: ShoppingCart = ShoppingCartSqlDelight(sqlDriver)
 
     override val userPreferences = UserPreferences(appContext)
 
---
+++ shopping-cart-sqldelight/src/main/java/com/handstandsam/shoppingapp/cart/SqlDelightShoppingCartDao.kt
@@ -0,0 +1,113 @@
+package com.handstandsam.shoppingapp.cart
+
+import com.handstandsam.shoppingapp.cart.sqldelight.Database
+import com.handstandsam.shoppingapp.models.Item
+import com.handstandsam.shoppingapp.models.ItemWithQuantity
+import com.squareup.sqldelight.Query
+import com.squareup.sqldelight.db.SqlDriver
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.launch
+
+/**
+ * A SqlDelight Implementation of our [ShoppingCartDao] to read and write to the Database
+ */
+class SqlDelightShoppingCartDao(sqlDriver: SqlDriver) :
+    CoroutineScope by CoroutineScope(Dispatchers.IO),
+    ShoppingCartDao {
+
+    /**
+     * Exposed as a [ReceiveChannel] for consumers to listen to updates to the Database contents
+     */
+    private val channel = ConflatedBroadcastChannel<List<ItemWithQuantity>>(listOf())
+
+    /**
+     * The SqlDelight queries that were created during code generation from our .sq file
+     */
+    private val itemInCartEntityQueries = Database(sqlDriver).itemInCartEntityQueries
+
+    override suspend fun selectAll(): List<ItemWithQuantity> {
+        return itemInCartEntityQueries.selectAll()
+            .executeAsList()
+            .toItemWithQuantityList()
+    }
+
+    override suspend fun selectAllStream(): ReceiveChannel<List<ItemWithQuantity>> {
+        return channel.openSubscription()
+    }
+
+    override suspend fun findByLabel(label: String): ItemWithQuantity? {
+        return itemInCartEntityQueries.selectByLabel(label)
+            .executeAsOneOrNull()
+            ?.toItemWithQuantity()
+    }
+
+    override suspend fun upsert(itemInCart: ItemWithQuantity) {
+        itemInCartEntityQueries.insertOrReplace(
+            label = itemInCart.item.label,
+            image = itemInCart.item.image,
+            link = itemInCart.item.link,
+            quantity = itemInCart.quantity
+        )
+    }
+
+    override suspend fun remove(itemInCart: ItemWithQuantity) {
+        itemInCartEntityQueries.deleteByLabel(itemInCart.item.label)
+    }
+
+    override suspend fun empty() {
+        itemInCartEntityQueries.empty()
+    }
+
+    /**
+     * Publishes the current [List] of [ItemWithQuantity] to the [ReceiveChannel]
+     */
+    private fun sendUpdateToChannel() {
+        launch {
+            channel.send(selectAll())
+        }
+    }
+
+    /**
+     * Listener that will get fired if the db is updated, so we can notify the [ReceiveChannel]
+     */
+    private val changeListener = object : Query.Listener {
+        override fun queryResultsChanged() {
+            sendUpdateToChannel()
+        }
+    }
+
+    /**
+     * We need to register our listener for updates, and publish our latest contents to the channel on initialization
+     */
+    init {
+        itemInCartEntityQueries.selectAll().addListener(changeListener) //Listens for updates
+        sendUpdateToChannel() //This is required so that we put the current contents on the channel
+    }
+
+}
+
+/**
+ * Extension Function converting the SqlDelight Model into our App's Model
+ */
+private fun ItemInCart.toItemWithQuantity(): ItemWithQuantity {
+    return ItemWithQuantity(
+        item = Item(
+            label = label,
+            image = image,
+            link = link
+        ),
+        quantity = quantity
+    )
+}
+
+/**
+ * Extension Function mapping the SqlDelight Model into our App's Model
+ */
+private fun List<ItemInCart>.toItemWithQuantityList(): List<ItemWithQuantity> {
+    return this.map {
+        it.toItemWithQuantity()
+    }
+}
