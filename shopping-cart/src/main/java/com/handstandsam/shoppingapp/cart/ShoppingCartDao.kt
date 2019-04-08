a099e8aad1811e1b0ceb22d9959d85d7d6ba9c64
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2019-04-07 20:59:36 -0400

    Added javadoc.

--- shopping-cart/src/main/java/com/handstandsam/shoppingapp/cart/ShoppingCartDao.kt
+++ shopping-cart/src/main/java/com/handstandsam/shoppingapp/cart/ShoppingCartDao.kt
@@ -3,6 +3,9 @@ package com.handstandsam.shoppingapp.cart
 import com.handstandsam.shoppingapp.models.ItemWithQuantity
 import kotlinx.coroutines.channels.ReceiveChannel
 
+/**
+ * All actions to access and modify our [ShoppingCart] database.
+ */
 interface ShoppingCartDao {
 
     suspend fun selectAll(): List<ItemWithQuantity>
