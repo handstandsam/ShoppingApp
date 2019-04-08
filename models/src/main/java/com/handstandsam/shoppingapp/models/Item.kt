ec50de3ac716a32e5e5d88096733deda632985b4
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2019-04-07 21:13:52 -0400

    Formatting that is more readable.

--- models/src/main/java/com/handstandsam/shoppingapp/models/Item.kt
+++ models/src/main/java/com/handstandsam/shoppingapp/models/Item.kt
@@ -2,4 +2,8 @@ package com.handstandsam.shoppingapp.models
 
 import java.io.Serializable
 
-data class Item(val label: String, val image: String, val link: String? = null) : Serializable
+data class Item(
+    val label: String,
+    val image: String,
+    val link: String? = null
+) : Serializable
