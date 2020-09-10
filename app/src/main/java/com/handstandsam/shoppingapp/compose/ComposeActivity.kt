19fe02282dadd37b01e6a7d1b80cb1a39e8a17a0
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2020-09-09 22:14:56 -0400

    Currently broken.  If you use `HomeActivity`, it reverts to old working behavior.
     use ./gradlew app:assembleInmemoryDebug to replicate Jetpack compose issue.

--- app/src/main/java/com/handstandsam/shoppingapp/compose/ComposeActivity.kt
+++ app/src/main/java/com/handstandsam/shoppingapp/compose/ComposeActivity.kt
@@ -3,36 +3,15 @@ package com.handstandsam.shoppingapp.compose
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
-import androidx.compose.runtime.Providers
 import androidx.compose.ui.platform.setContent
-import com.github.zsoltk.compose.backpress.AmbientBackPressHandler
-import com.github.zsoltk.compose.backpress.BackPressHandler
 
 class ComposeActivity : AppCompatActivity() {
-
-//    override fun onBackPressed() {
-//        if (!backPressHandler.handle()) {
-//            super.onBackPressed()
-//        }
-//    }
-
-//    private val backPressHandler = BackPressHandler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             Text("hi")
-//            MyApplicationTheme {
-//                Providers(
-//                    AmbientBackPressHandler provides backPressHandler
-//                ) {
-//                    // Your root composable goes here
-//                    Root.Content(Root.Routing.UsMap)
-//                }
-//            }
         }
     }
 }
-
-
