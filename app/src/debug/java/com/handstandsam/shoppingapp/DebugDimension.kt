5e1e801210c64ae916660010ae189007843a84b3
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2021-08-14 14:43:03 -0400

    Removed Gander

--- app/src/debug/java/com/handstandsam/shoppingapp/DebugDimension.kt
+++ app/src/debug/java/com/handstandsam/shoppingapp/DebugDimension.kt
@@ -2,22 +2,16 @@ package com.handstandsam.shoppingapp
 
 import android.app.Application
 import android.content.Context
-import com.ashokvarma.gander.Gander
-import com.ashokvarma.gander.GanderInterceptor
-import com.ashokvarma.gander.imdb.GanderIMDB
 import okhttp3.Interceptor
 import okhttp3.logging.HttpLoggingInterceptor
 import timber.log.Timber
 
 fun debugDimensionInterceptors(appContext: Context): List<Interceptor> {
     return listOf(
-        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY),
-        GanderInterceptor(appContext).showNotification(true)
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
     )
 }
 
 fun Application.debugDimensionOnCreate() {
-    // For In Memory DB (Data retained in memory lost on app close)
-    Gander.setGanderStorage(GanderIMDB.getInstance());
     Timber.plant(Timber.DebugTree())
 }
