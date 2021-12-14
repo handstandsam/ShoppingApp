63cfc1ff4059ee55560f0a28fcf38ab3ca795b1a
Sam Edwards <samedwards@dropbox.com>
2021-12-14 09:46:12 -0500

    AAR -> JAR

--- mock-data/build.gradle.kts
+++ mock-data/build.gradle.kts
@@ -1,17 +1,6 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
-}
-
-android {
-    compileSdkVersion(Versions.compile_sdk)
-
-    defaultConfig {
-        minSdkVersion(Versions.min_sdk)
-        targetSdkVersion(Versions.target_sdk)
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+    id("java-library")
+    id("kotlin")
 }
 
 dependencies {
