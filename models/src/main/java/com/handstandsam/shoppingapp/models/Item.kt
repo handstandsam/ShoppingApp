183f4ccd60671c26f08900561ed65c6afa4c7232
Sam Edwards <samedwards@dropbox.com>
2021-12-14 11:04:37 -0500

    Moving @Serializable code to the Android module in prep for multiplatform support.

--- models/src/main/java/com/handstandsam/shoppingapp/models/Item.kt
+++ models/src/main/java/com/handstandsam/shoppingapp/models/Item.kt
@@ -1,9 +1,7 @@
 package com.handstandsam.shoppingapp.models
 
-import java.io.Serializable
-
 data class Item(
     val label: String,
     val image: String,
     val link: String? = null
-) : Serializable
+)
