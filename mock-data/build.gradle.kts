01057b41087fa68ed4df97d6730cd7a59e809354
Sam Edwards <samedwards@dropbox.com>
2021-12-14 09:43:38 -0500

    build.gradle -> build.gradle.kts

---
+++ mock-data/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    compileSdkVersion(Versions.compile_sdk)
+
+    defaultConfig {
+        minSdkVersion(Versions.min_sdk)
+        targetSdkVersion(Versions.target_sdk)
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    implementation(project(Modules.models))
+    implementation(Libs.kotlin_std_lib)
+}
