fff9a77a9c19cc200c3002f9cf58f1b83c4777bd
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2022-01-16 15:28:04 -0500

    Pokeball!

--- app/src/main/java/com/handstandsam/shoppingapp/features/poke/PokeActivity.kt
+++ app/src/main/java/com/handstandsam/shoppingapp/features/poke/PokeActivity.kt
@@ -1,32 +1,11 @@
-package com.handstandsam.shoppingapp.features.countby
+package com.handstandsam.shoppingapp.features.poke
 
-import android.icu.util.UniversalTimeScale.toBigDecimal
-import android.icu.util.UniversalTimeScale.toLong
 import android.os.Build
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Slider
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.handstandsam.shoppingapp.di.AppGraph
@@ -34,10 +13,9 @@ import com.handstandsam.shoppingapp.features.itemdetail.ItemDetailActivity
 import com.handstandsam.shoppingapp.graph
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import java.math.BigInteger
 import java.util.Locale
 
-class CountByActivity : ComponentActivity() {
+class PokeActivity : ComponentActivity() {
 
     private val graph: AppGraph get() = application.graph()
 
@@ -71,9 +49,28 @@ class CountByActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val categoryViewModel = ViewModelProvider(this, graph.viewModelFactory)
+            .get(PokeViewModel::class.java)
+
+
         setContent {
-            CountBy { speak(it) }
+            Pokeball ()
         }
 
+        lifecycleScope.launchWhenCreated {
+            categoryViewModel.sideEffects
+                .onEach {
+                    when (it) {
+                        is PokeViewModel.SideEffect.LaunchItemDetailActivity -> {
+                            ItemDetailActivity.launch(this@PokeActivity, it.item)
+                        }
+                    }
+                }
+                .launchIn(this)
+        }
+
+    }
+
+    companion object {
     }
 }
