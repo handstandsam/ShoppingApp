028701e9b96d13137380497777009a7fed7ff18d
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2022-01-17 08:05:38 -0500

    Bouncy!

--- app/src/main/java/com/handstandsam/shoppingapp/features/poke/PokeActivity.kt
+++ app/src/main/java/com/handstandsam/shoppingapp/features/poke/PokeActivity.kt
@@ -6,8 +6,11 @@ import android.speech.tts.TextToSpeech
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,13 +22,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.handstandsam.shoppingapp.R
 import com.handstandsam.shoppingapp.di.AppGraph
 import com.handstandsam.shoppingapp.features.itemdetail.ItemDetailActivity
 import com.handstandsam.shoppingapp.graph
@@ -71,32 +79,94 @@ class PokeActivity : ComponentActivity() {
             .get(PokeViewModel::class.java)
 
         setContent {
-            var currentOffset by remember { mutableStateOf(Offset(0f, 0f)) }
+            val configuration = LocalConfiguration.current
+            val screenWidth = with(LocalDensity.current) { configuration.screenWidthDp.dp.toPx() }
+            val screenHeight = with(LocalDensity.current) { configuration.screenHeightDp.dp.toPx() }
+
+
+            val pokeballSizeDp by remember { mutableStateOf(150.dp) }
+            val pokeballSizePx =
+                with(LocalDensity.current) {
+                    Size(
+                        width = pokeballSizeDp.toPx(),
+                        height = pokeballSizeDp.toPx()
+                    )
+                }
+            val originalOffset = Offset(
+                x = (screenWidth / 2) - (pokeballSizePx.width / 2),
+//                y = screenHeight - pokeballSizePx.height - 200,
+                y = (screenHeight / 2) - (pokeballSizePx.height / 2),
+            )
+
+            var isDragging by remember { mutableStateOf(false) }
+            var currentOffset by remember { mutableStateOf(originalOffset) }
+
+            var animationSpec = spring<Float>(dampingRatio = Spring.DampingRatioMediumBouncy)
+            val offsetAnimationX: Float by animateFloatAsState(
+                currentOffset.x,
+                animationSpec,
+            )
+
+            val offsetAnimationY: Float by animateFloatAsState(
+                currentOffset.y,
+                animationSpec,
+            )
+
             Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .pointerInput(Unit) {
-                        detectDragGestures { change, dragAmount ->
-                            println("$change $dragAmount")
-                            change.consumeAllChanges()
-                            val x = (currentOffset.x + dragAmount.x)
-                                .coerceIn(0f, size.width.toFloat())
-                            val y = (currentOffset.y + dragAmount.y)
-                                .coerceIn(0f, size.height.toFloat())
-                            currentOffset = Offset(x, y)
-                        }
-                    }
-                    .background(Color.Yellow)
-                    .padding(8.dp)
-            ) {
-                Pokeball(
-                    sizedp = 150.dp,
-                    modifier = Modifier
-                        .offset(
-                            x = with(LocalDensity.current) { currentOffset.x.toDp() },
-                            y = with(LocalDensity.current) { currentOffset.y.toDp() }
+                        detectDragGestures(
+                            onDrag = { change, dragAmount ->
+                                println("$change $dragAmount")
+                                change.consumeAllChanges()
+                                val x = (currentOffset.x + dragAmount.x)
+                                    .coerceIn(0f, size.width.toFloat() - pokeballSizePx.width)
+                                val y = (currentOffset.y + dragAmount.y)
+                                    .coerceIn(0f, size.height.toFloat() - pokeballSizePx.height)
+                                currentOffset = Offset(x, y)
+                            },
+                            onDragStart = {
+                                isDragging = true
+                            },
+                            onDragEnd = {
+//                                isDragging = false
+                                currentOffset = originalOffset
+                            }
                         )
+                    }
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.pokecatchbackground),
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.FillBounds,
+                    contentDescription = "content description"
                 )
+
+                with(LocalDensity.current) {
+                    Size(
+                        width = pokeballSizeDp.toPx(),
+                        height = pokeballSizeDp.toPx()
+                    )
+                }
+                val offset =
+//                    if (isDragging) {
+//                        Pair(currentOffset.x, currentOffset.y)
+//                    } else {
+                    Pair(offsetAnimationX, offsetAnimationY)
+//                    }
+                with(LocalDensity.current) {
+                    Pokeball(
+                        sizedp = pokeballSizeDp,
+                        modifier = Modifier
+                            .offset(
+//                            x = currentOffset.x.dp,
+//                            y = currentOffset.y.dp,
+                                x = offset.first.toDp(),
+                                y = offset.second.toDp(),
+                            )
+                    )
+                }
             }
         }
 
