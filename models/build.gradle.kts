27ac3558a8d0951e7b74cd3711b9f773745f0914
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2022-04-17 13:38:11 -0400

    Removed config for #39

--- models/build.gradle.kts
+++ models/build.gradle.kts
@@ -1,40 +1,7 @@
 plugins {
     kotlin("jvm")
-    `java-library`
-}
-
-sourceSets {
-    main {
-        java {
-            srcDir("src/commonMain/kotlin")
-        }
-    }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
 }
-
-// TODO Re-enable Multiplatform https://github.com/handstandsam/ShoppingApp/issues/39
-//plugins {
-//    kotlin("multiplatform")
-//}
-//
-//kotlin {
-//    jvm {
-//        compilations.all {
-//            kotlinOptions.jvmTarget = "1.8"
-//        }
-//        testRuns["test"].executionTask.configure {
-//            useJUnit()
-//        }
-//    }
-//
-//    sourceSets {
-//        val commonMain by getting {
-//            dependencies {
-//                implementation(kotlin("stdlib"))
-//            }
-//        }
-//    }
-//}
