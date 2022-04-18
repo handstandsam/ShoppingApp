400edf9a0d105af7f0e2248f52397e1efcbe814a
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2022-04-17 20:21:43 -0400

    Moved back from commonMain to main

--- mock-data/build.gradle.kts
+++ mock-data/build.gradle.kts
@@ -3,40 +3,7 @@ plugins {
     `java-library`
 }
 
-sourceSets {
-    main {
-        java {
-            srcDir("src/commonMain/kotlin")
-        }
-    }
-}
-
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(project(":models"))
 }
-
-// TODO Re-enable Multiplatform https://github.com/handstandsam/ShoppingApp/issues/39
-//plugins {
-//    kotlin("multiplatform")
-//}
-//
-//kotlin {
-//    jvm {
-//        compilations.all {
-//            kotlinOptions.jvmTarget = "1.8"
-//        }
-//        testRuns["test"].executionTask.configure {
-//            useJUnit()
-//        }
-//    }
-//
-//    sourceSets {
-//        val commonMain by getting {
-//            dependencies {
-//                implementation(kotlin("stdlib"))
-//                implementation(project(":models"))
-//            }
-//        }
-//    }
-//}
--- models/build.gradle.kts
+++ models/build.gradle.kts
@@ -3,14 +3,6 @@ plugins {
     `java-library`
 }
 
-sourceSets {
-    main {
-        java {
-            srcDir("src/commonMain/kotlin")
-        }
-    }
-}
-
 dependencies {
     implementation(kotlin("stdlib"))
 }
