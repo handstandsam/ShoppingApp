a78216151c41f5a9ac3b134d02fbe3f1093a6a62
Sam Edwards <sam@handstandtech.com>
2022-05-05 12:25:22 -0400

    Better support for the use of the "main" sourceset as "commonMain".

--- convention-plugins/src/main/kotlin/com.handstandsam.jvm.lib.gradle.kts
+++ convention-plugins/src/main/kotlin/com.handstandsam.jvm.lib.gradle.kts
@@ -1,16 +1,42 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
 plugins {
     kotlin("multiplatform")
 }
 
 kotlin {
+    jvm()
     sourceSets {
-        val main by creating {
-            kotlin.srcDir("src/main/java")
-        }
         val commonMain by getting {
-            dependsOn(main)
+            kotlin.srcDirs("src/main/java", "src/main/kotlin")
         }
+
+        val commonTest by getting {
+            kotlin.srcDirs("src/test/java", "src/test/kotlin")
+        }
+
+        val implementation by configurations.creating
+        val testImplementation by configurations.creating
+
+        configurations.getByName(commonMain.implementationConfigurationName).extendsFrom(implementation)
+        configurations.getByName(commonTest.implementationConfigurationName).extendsFrom(testImplementation)
+
+        fun createAliasConfigurations(sourceSet: KotlinSourceSet) {
+            sourceSet.relatedConfigurationNames.forEach { configurationName ->
+                val aliasName = configurationName
+                    .removePrefix("common")
+                    .removePrefix("Main")
+                    .decapitalize()
+
+                val configuration = configurations.getByName(configurationName)
+                val aliasConfiguration = configurations.maybeCreate(aliasName)
+                configuration.extendsFrom(aliasConfiguration)
+            }
+        }
+
+        createAliasConfigurations(commonMain)
+        createAliasConfigurations(commonTest)
     }
 
-    jvm {}
+    println(configurations.map { it.name }.sorted())
 }
