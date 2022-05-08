8f9e265cb4c97eb92bc7ba10aef8962b93b6ff36
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2022-05-08 14:52:50 -0400

    Trying github actions.
    Fixed aar -> multiplatform compatible, added github actions and dependency-guard 0.2.0-SNAPSHOT
    
    Enabled Gradle Build Scans
    
    Moving these changes to another PR.

--- app-flavor-mockserver/src/main/java/com/handstandsam/shoppingapp/mockaccount/EndpointUrls.kt
+++ app-flavor-mockserver/src/main/java/com/handstandsam/shoppingapp/mockaccount/EndpointUrls.kt
@@ -1,14 +1,16 @@
 package com.handstandsam.shoppingapp.mockaccount
 
 
-import android.net.Uri
 
 object EndpointUrls {
 
     const val categoriesUrl: String = "/categories"
 
     fun getItemsForCategoryUrl(categoryId: String): String {
-        return "/category/" + Uri.encode(categoryId) + "/items"
+        // I replaced Android Uri Encode with this "workaround"
+        // Issue https://github.com/handstandsam/ShoppingApp/issues/55
+        val encodedCategoryId = categoryId.replace("", "%20")
+        return "/category/$encodedCategoryId/items"
     }
 
     const val loginUrl: String = "/login"
