f0a44ae80956676756f051fbb666edaba2936864
Sam Edwards <sam@handstandtech.com>
2022-05-09 10:42:53 -0400

    Without Multiplatform - Kotlin Plugin

--- convention-plugins/src/main/kotlin/com/handstandsam/JvmLibConventionPlugin.kt
+++ convention-plugins/src/main/kotlin/com/handstandsam/JvmLibConventionPlugin.kt
@@ -5,15 +5,10 @@ import org.gradle.api.Project
 
 class JvmLibConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        val useMultiplatform = true
-        if (useMultiplatform) {
-            target.plugins.apply(JvmKmpLibConventionPlugin::class.java)
-        } else {
-            target.plugins.apply("org.jetbrains.kotlin.jvm")
-            target.dependencies.add(
-                "implementation",
-                "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.20"
-            )
-        }
+        target.plugins.apply("org.jetbrains.kotlin.jvm")
+        target.dependencies.add(
+            "implementation",
+            "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.20"
+        )
     }
 }
