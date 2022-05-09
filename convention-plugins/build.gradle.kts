7599db883d9cf737134b4b9e8076f8e0e7552908
Sam Edwards <sam@handstandtech.com>
2022-05-09 09:46:41 -0400

    Using updated Java Compilation Config

--- convention-plugins/build.gradle.kts
+++ convention-plugins/build.gradle.kts
@@ -11,4 +11,15 @@ repositories {
 dependencies {
     implementation(libs.kotlin.gradle.plugin)
     implementation(libs.android.gradle.plugin)
+    compileOnly(gradleApi())
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(8)
 }
