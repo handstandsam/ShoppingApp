ed118b45b2a33d44713084fe6c9544fc31e450b8
Sam Edwards <sam@handstandtech.com>
2022-05-20 16:24:14 -0400

    IOS Network Call with Ktor Darwin Engine (#67)
    * Using Darwin Engine on iOS to make HTTP calls.
    
    * Deleting this file that should not be tracked.

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,8 +2,26 @@ import SwiftUI
 import multiplatform
 
 struct ContentView: View {
+    
+    func something() {
+        let graph = IosMultiplatformApi().networkGraph.categoryRepo.getCategories {
+            data, error in
+            if let people = data {
+                print(people)
+            }
+            if let errorReal = error {
+                print(errorReal)
+            }
+        }
+    }
+    
+    init(){
+        something()
+    }
+
+    
     let greet = Greeting().greeting()
-    let user = User(firstname: "handstands", lastname: "sam")
+    let user = ModelsUser(firstname: "handstands", lastname: "sam")
 	var body: some View {
 		Text(user.description())
         Text(greet)
--- multiplatform/src/iosMain/kotlin/com/handstandsam/shoppingapp/multiplatform/Platform.kt
+++ multiplatform/src/iosMain/kotlin/com/handstandsam/shoppingapp/multiplatform/Platform.kt
@@ -1,11 +1,11 @@
 package com.handstandsam.shoppingapp.multiplatform
 
 import io.ktor.client.engine.HttpClientEngineFactory
-import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.darwin.Darwin
 import platform.UIKit.UIDevice
 
 actual class Platform actual constructor() {
     actual val platform: String =
         UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-    actual val ktorHttpClientEngine: HttpClientEngineFactory<*> = CIO
+    actual val ktorHttpClientEngine: HttpClientEngineFactory<*> = Darwin
 }
