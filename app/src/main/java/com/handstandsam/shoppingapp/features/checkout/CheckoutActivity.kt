f181bf2a21dbeed1b08c76d0d5497efd9328112b
Sam Edwards <sam@handstandtech.com>
2022-08-28 08:54:28 -0400

    Cleanup/Updates

--- app/src/main/java/com/handstandsam/shoppingapp/features/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/handstandsam/shoppingapp/features/checkout/CheckoutActivity.kt
@@ -4,36 +4,14 @@ import android.os.Bundle
 import android.view.Gravity
 import android.widget.Toast
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.handstandsam.shoppingapp.LoggedInActivity
-import com.handstandsam.shoppingapp.compose.ShoppingAppImageLoader
+import com.handstandsam.shoppingapp.compose.AndroidShoppingAppImageLoader
 import com.handstandsam.shoppingapp.compose.ShoppingCartScreen
-import com.handstandsam.shoppingapp.compose.loadNetworkImage
 import com.handstandsam.shoppingapp.features.home.AndroidShoppingCartViewModel
-import com.skydoves.landscapist.coil.CoilImage
-
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-
-
-class AndroidShoppingAppImageLoader : ShoppingAppImageLoader {
-    @Composable
-    override fun loadImage(modifier: Modifier, image: String) {
-        return CoilImage(
-            modifier = modifier,
-            imageModel = image,
-            // Crop, Fit, Inside, FillHeight, FillWidth, None
-            contentScale = ContentScale.Crop,
-            // shows an image with a circular revealed animation.
-            circularRevealedEnabled = true
-        )
-    }
-}
 
 class CheckoutActivity : LoggedInActivity() {
 
@@ -76,7 +54,8 @@ class CheckoutActivity : LoggedInActivity() {
                     )
                 },
                 logoutClicked = { logout() },
-                homeUpClicked = { onBackPressed() }
+                homeUpClicked = { onBackPressed() },
+                shoppingAppImageLoader = AndroidShoppingAppImageLoader()
             )
         }
     }
--- compose-ui/build.gradle.kts
+++ compose-ui/build.gradle.kts
@@ -1,5 +1,4 @@
 import org.jetbrains.compose.compose
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("multiplatform") // kotlin("jvm") doesn't work well in IDEA/AndroidStudio (https://github.com/JetBrains/compose-jb/issues/22)
@@ -10,18 +9,17 @@ kotlin {
     jvm {
         withJava()
     }
-    sourceSets {
-        named("jvmMain") {
-            dependencies {
-                compileOnly(compose.ui)
-                compileOnly(compose.foundation)
-                compileOnly(compose.material)
-                implementation(libs.kotlin.coroutines)
-                implementation(project(":models"))
-                implementation(project(":mock-data"))
-                implementation(project(":shopping-cart"))
-                implementation(project(":networking"))
-            }
+    val jvmMain by sourceSets.getting {
+        dependencies {
+            implementation(compose.ui)
+            implementation(compose.foundation)
+            implementation(compose.material)
+
+            implementation(project(":models"))
+            implementation(project(":mock-data"))
+            implementation(project(":shopping-cart"))
+            implementation(project(":networking"))
+            implementation(libs.kotlin.coroutines)
         }
     }
 }
